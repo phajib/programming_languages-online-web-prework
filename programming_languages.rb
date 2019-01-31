7f5de8838c361473bac352d3c46a0c0224922787
H B <phajib@gmail.com>
2019-01-31 10:03:25 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -3,7 +3,7 @@ def reformat_languages(languages)
   languages.each do |lang_name, describe|
     describe.each do |style, data|
       if new_hash[lang_name] == nil
-        new_hash[lang_name] = describe
+        new_hash[lang_name] = data
         new_hash[lang_name][:style] = []
       end
       new_hash[lang_name][:style] << style
