e7ba5987e03112793f369e84aa05e27fbffb9689
H B <phajib@gmail.com>
2019-02-01 08:48:51 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,17 +1,11 @@
 def reformat_languages(languages)
-  new_hash = {} #build a new hash
-  languages.each do |style, type| #style: oo, functional. data: hash
-    type.each do |prog_lang, attribute| #lang_name: ruby, etc. describe: hash
-       #set lang_name to be the key on top level
-       #the value will be the hash(type=>...)
+  new_hash = {} 
+  languages.each do |style, type|
+    type.each do |prog_lang, attribute|
        if new_hash[prog_lang] == nil
           new_hash[prog_lang] = attribute
         end
-          #set style key empty so we can add the style later
-          new_hash[prog_lang][:style] = []
-        #end
-        #add style to new_hash
-        #if the same lang_name, it will go straight here and add style
+        new_hash[prog_lang][:style] = []
         new_hash[prog_lang][:style] << style
     end
   end
